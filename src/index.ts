c3b3f72481b10162351f9b56d7d56f624c9d7425
TheBoringDude <iamcoderx@gmail.com>
2021-12-09 14:35:40 +0800

    Fix missing export of `useGetCurrencyBalance`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import UseEOSProvider from './component/provider'
+import useGetCurrencyBalance from './hooks/useGetCurrencyBalance'
 import useGetTableRows from './hooks/useGetTableRows'
 
-export { UseEOSProvider, useGetTableRows }
+export { UseEOSProvider, useGetTableRows, useGetCurrencyBalance }
