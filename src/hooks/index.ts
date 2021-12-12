36d3da813d3f26c82eb7ef4f59fc7643f3b84fa5
TheBoringDude <iamcoderx@gmail.com>
2021-12-12 19:13:59 +0800

    Fix way of importing and exporting main hooks functions.

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+import useGetAccount from './useGetAccount'
+import useGetCurrencyBalance from './useGetCurrencyBalance'
+import useGetTableRows from './useGetTableRows'
+
+export { useGetAccount, useGetCurrencyBalance, useGetTableRows }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import UseEOSProvider from './component/provider'
-import useGetCurrencyBalance from './hooks/useGetCurrencyBalance'
-import useGetTableRows from './hooks/useGetTableRows'
 
-export { UseEOSProvider, useGetTableRows, useGetCurrencyBalance }
+export * from './hooks'
+export { UseEOSProvider }
